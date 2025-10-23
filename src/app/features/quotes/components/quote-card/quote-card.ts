c6267fb82fdd214ab08060da9a7caf3f457eb2e2
Mansur Isakov <mansur.i@payme.uz>
2025-10-23 16:31:31 +0500

    feat: icons lib

--- src/app/features/quotes/components/quote-card/quote-card.ts
+++ src/app/features/quotes/components/quote-card/quote-card.ts
@@ -1,11 +1,12 @@
-// src/app/components/quote-card/quote-card.component.ts
 import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from '@angular/core';
 import { Quote } from '../../types/quote';
+import { LucideAngularModule, HeartIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-quote-card',
   templateUrl: './quote-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [LucideAngularModule],
 })
 export class QuoteCardComponent {
   @Input() quote!: Quote;
@@ -17,6 +18,7 @@ export class QuoteCardComponent {
   @Output() share = new EventEmitter<'twitter' | 'facebook'>();
 
   showRating = false;
+  HeartIcon = HeartIcon;
 
   onRate(r: number) {
     this.rate.emit(r);
